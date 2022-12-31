677a32ee3424f45ba431e6d2ca6ca46df33474d0
YaroslavSmirnov1 <89937201+YaroslavSmirnov1@users.noreply.github.com>
2022-12-31 16:43:27 +0300

    1
    1

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ routes = web.RouteTableDef()
 @routes.post('/exchange/{base}/{amount}/{sym}')
 async def exchange(request):
     try:
-        base = request.match_info.get('base')
+        base = request.match_info['base']
     except KeyError:
         response_obj = {'status': 'failed', 'reason': "Конвертируемая валюта не найдена!"}
         return web.Response(text=json.dumps(response_obj, ensure_ascii=False), status=400)
     try:
-        sym = request.match_info.get('sym')
+        sym = request.match_info['sym']
     except KeyError:
         response_obj = {'status': 'failed', 'reason': "Валюта, в которую Вы пытаетесь конвертировать, не найдена!"}
         return web.Response(text=json.dumps(response_obj, ensure_ascii=False), status=400)
     try:
-        amount = request.match_info.get('amount')
+        amount = request.match_info['amount']
     except KeyError:
         response_obj = {'status': 'failed', 'reason': 'Не удалось обработать количество!'}
         return web.Response(text=json.dumps(response_obj, ensure_ascii=False), status=400)
@@ -30,13 +30,10 @@ async def exchange(request):
     except ValueError:
         response_obj = {'status': 'failed', 'reason': "Введите количество валюты в числовом эквиваленте!"}
         return web.Response(text=json.dumps(response_obj, ensure_ascii=False), status=400)
-    try:
-        result = f"Цена {amount} {base} в {sym} : {Converter.get_price(base, amount, sym)}"
-        response_obj = str(result)
-        return web.Response(text=response_obj, status=200)
-    except Exception as e:
-        response_obj = {'status': 'failed', 'reason': str(e)}
-        return web.Response(text=json.dumps(response_obj, ensure_ascii=False))
+    result = f"Цена {amount} {base} в {sym} : {Converter.get_price(base, amount, sym)}"
+    response_obj = str(result)
+    return web.Response(text=response_obj, status=200)
+
 
 app = web.Application()
 app.add_routes(routes)
